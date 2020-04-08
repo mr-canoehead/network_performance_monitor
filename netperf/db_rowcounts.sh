ecf8283b50d888a9ed7ca0bdcc25ab8bf494748d
Chris <mr_canoehead@telus.net>
2020-04-07 21:56:10 -0700

    added script to list database table row counts

---
+++ netperf/db_rowcounts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This file is part of the Network Performance Monitor which is released under the GNU General Public License v3.0
+# See the file LICENSE for full license details.
+
+declare -a tables
+
+tables+=( "bandwidth" )
+tables+=( "ping" )
+tables+=( "iperf3" )
+tables+=( "dns" )
+tables+=( "speedtest" )
+tables+=( "isp_outages" )
+
+client_id=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}' | sed 's/^0*//')
+database="/mnt/usb_storage/netperf/$client_id/database/$client_id.db"
+printf "\nDatabase file: %s\n\n" "$database"
+printf "%-12s %s\n" "Table" "Rowcount"
+printf "%-12s %s\n" "-----" "--------"
+
+for table in "${tables[@]}"
+do
+	rowcount=$( sqlite3 "$database"	"select count(*) from $table;" )
+	printf "%-12s %s\n" "$table" "$rowcount"
+done
