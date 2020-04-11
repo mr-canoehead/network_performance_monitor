ea1890873aa1b8881664bb91afac03095abc7900
Chris <mr_canoehead@telus.net>
2020-04-11 06:08:00 -0700

    removed unnecessary formatting steps, it could all be done via printf format specifiers.

--- netperf/tools/bridge_watcher.sh
+++ netperf/tools/bridge_watcher.sh
@@ -14,19 +14,9 @@ while true; do
 	rx_tx_bps=$( sqlite3 "$database" "select rx_bps,tx_bps from bandwidth WHERE epoch_time = (SELECT MAX(epoch_time)  FROM bandwidth);" )
 	rx_bps=$( echo "$rx_tx_bps" | awk -F'|' '{print $1}' )
 	tx_bps=$( echo "$rx_tx_bps" | awk -F'|' '{print $2}' )
-	rx_Mbps=$( echo "$rx_bps/1000000" | bc -l )
-	tx_Mbps=$( echo "$tx_bps/1000000" | bc -l )
-	if [[ "$rx_Mbps" == "0" ]]; then
-		rx_Mbps="0.0000"
-	else
-		rx_Mbps=$( printf "%0.4f" "$rx_Mbps" )
-	fi
-	if [[ "$tx_Mbps" == "0" ]]; then
-		tx_Mbps="0.0000"
-	else
-		tx_Mbps=$( printf "%0.4f" "$tx_Mbps" )
-	fi
-	printf "\r%7s  %7s" "$rx_Mbps" "$tx_Mbps"
+	rx_Mbps=$( echo "$rx_bps/10^6" | bc -l )
+	tx_Mbps=$( echo "$tx_bps/10^6" | bc -l )
+	printf "\r%7.4f  %7.4f" "$rx_Mbps" "$tx_Mbps"
 	read -t 0.25 -N 1 input
 	if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
 		echo
