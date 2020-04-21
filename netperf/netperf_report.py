fd073ad21cc6fc820faecee80afcc09ae0051003
Chris <mr_canoehead@telus.net>
2020-04-20 17:31:50 -0700

    refactored the report generation process to use pgfkeys rather than the Python-based string-replacement method used previously.

--- netperf/netperf_report.py
+++ netperf/netperf_report.py
@@ -7,6 +7,7 @@ from datetime import datetime,date,timedelta
 import time
 import sys
 import os
+import shutil
 from subprocess import check_output,Popen,STDOUT,PIPE
 import matplotlib
 matplotlib.use('Agg')
@@ -27,6 +28,7 @@ CLIENT_ID = util.get_client_id()
 DATA_ROOT="{}/{}".format(NETPERF_SETTINGS.get_data_root(),CLIENT_ID)
 NETPERF_DB=NETPERF_SETTINGS.get_db_filename()
 REPORT_TEMPLATE_PATH="/opt/netperf/templates"
+REPORT_TEMPLATE_FILENAME="netperf_report_template.tex"
 REPORTS_PATH="{}/reports".format(DATA_ROOT)
 TMP_PATH="{}/tmp".format(REPORTS_PATH)
 
@@ -58,7 +60,23 @@ def align_yaxis(ax1, v1, ax2, v2):
     miny, maxy = ax2.get_ylim()
     ax2.set_ylim(miny+dy, maxy+dy)
 
+class pgf_keyvals:
+        def __init__ (self):
+                self.keyvalues = []
+
+        def add(self,key,value):
+                self.keyvalues.append({ "key" : key, "value" : value })
+
+        def __str__(self):
+                output = ""
+                for kv in self.keyvalues:
+                        key = kv["key"]
+                        value = kv["value"]
+                        output += "\\pgfkeyssetvalue{{{}}}{{{}}}\n".format(key,value)
+                return output
+
 def main():
+	report_keyvals = pgf_keyvals()
 	main_replacement_values = {}
 	today = date.today()
 	midnight = datetime.combine(today,datetime.max.time())
@@ -80,9 +98,9 @@ def main():
 
 	report_log.info("Generating network performance report for date {}".format(query_date.strftime("%Y-%m-%d")))
 
-	main_replacement_values["<CLIENT_ID>"] = CLIENT_ID
-	main_replacement_values["<QUERY_DATE>"] = query_date.strftime("%Y-%m-%d")
-	main_replacement_values["<GRAPHICS_PATH>"] = TMP_PATH
+	report_keyvals.add("main/client_id", CLIENT_ID)
+	report_keyvals.add("main/query_date", query_date.strftime("%Y-%m-%d"))
+	report_keyvals.add("main/graphics_path", TMP_PATH)
 
 	db = netperf_db(NETPERF_DB)
 
@@ -142,8 +160,7 @@ def main():
 
 	isp_outages["y_values"] = np.zeros_like(isp_outages["times"])
 
-
-	main_replacement_values["<ISP_OUTAGES>"] = str(len(isp_outage_rows))
+	report_keyvals.add("main/isp_outages", str(len(isp_outage_rows)))
 
 	speedtest_data["outages"]["y_values"] = np.zeros_like(speedtest_data["outages"]["times"])
 	speedtest_data["averages"] = {}
@@ -171,7 +188,6 @@ def main():
 	linesum = lines["rx"] + lines["tx"] + lines["ping"]
 
 	if len(rows) == 0:
-		### TEXT TEST
 		axes["ping"].text(3,0,"There are no speedtest results for the reporting day.")
 
 	if len(isp_outages["times"]) > 0:
@@ -192,31 +208,37 @@ def main():
 
 	if len(isp_outages["times"]) > 0:
 		speedtest_data["outages"]["info"] = "One or more times during the reporting day an Internet outage was recorded."
-		#speedtest_data["outages"]["info"] = "One or more times during the reporting day an Internet outage was recorded.\\footnote{{An internet outage is logged when the system fails to ping the remote host '8.8.8.8'. This address is Google's primary DNS server which is an extremely reliable service. A failure to ping this remote host is a strong indication that Internet connectivity has been interrupted.}} This may indicate that an interruption occurred with your Internet service."
 	else:
 		speedtest_data["outages"]["info"] = "No Internet outages were recorded during the reporting day."
-
-	main_replacement_values["<OUTAGE_INFO>"] = speedtest_data["outages"]["info"]
+	report_keyvals.add("main/outage_info", speedtest_data["outages"]["info"])
 	legend_labels = [l.get_label() for l in linesum]
 	axes["rx_tx"].legend(linesum,legend_labels,loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=legend_columns)
 	chart_filename = "speedtest_chart.pdf"
-	main_replacement_values["<SPEEDTEST_CHART_NAME>"] = chart_filename
+	report_keyvals.add("main/speedtest_chart_name", chart_filename)
 	fig.savefig("{}/{}".format(TMP_PATH,chart_filename),format='pdf', bbox_inches='tight')
 	plt.cla()
 
-	with open("{}/netperf_report_template.tex".format(REPORT_TEMPLATE_PATH)) as f:
-		report = f.read()
+	# copy report template to the working directory
+	try:
+		shutil.copyfile("{}/{}".format(REPORT_TEMPLATE_PATH,REPORT_TEMPLATE_FILENAME),"{}/{}".format(TMP_PATH,REPORT_TEMPLATE_FILENAME))
+	except IOError:
+		report_log.error("Failed to copy report template file to the working directory.")
+		sys.exit(1)
+
+	###with open("{}/netperf_report_template.tex".format(REPORT_TEMPLATE_PATH)) as f:
+	###	report = f.read()
 
 	# generate LaTeX strings that will be used to print the speedtest data rows
 	speedtest_data["table_tex"] = ""
 	for r in rows:
 		speedtest_data["table_tex"] += "{} & {} & {} & {} & {}\\\\\n".format(datetime.fromtimestamp(r["timestamp"]).strftime("%H:%M"),r["rx_Mbps"],r["tx_Mbps"],r["ping"],r["remote_host"])
-	main_replacement_values["<SPEEDTEST_TABLE_DATA>"] = speedtest_data["table_tex"]
+	report_keyvals.add("main/speedtest_table_data", speedtest_data["table_tex"])
 
 	rows = db.get_bandwidth_data(query_date)
 
 	if len(rows) > 0:
 		bin_width = 10
+		report_keyvals.add("bwmonitor/bin_width", str(bin_width))
 		rx_tbins = time_bins(bin_width)
 		tx_tbins = time_bins(bin_width)
 		bandwidth_data = {}
@@ -243,10 +265,7 @@ def main():
 		axes["times"].set_xlabel('Time of day (24 hour clock)')
 		axes["times"].set_ylabel('Bandwidth (Mbps)')
 		lines={}
-		#lines["rx"] = axes["times"].bar(rx_tbins.get_times(),rx_tbins.get_means(),width=0.2,color="xkcd:blue",label='Receive')
-		#lines["tx"] = axes["times"].bar(tx_tbins.get_times(),rx_tbins.get_means(),color="xkcd:green",label='Transmit')
 		lines["rx"] = axes["times"].plot(rx_tbins.get_times(),rx_tbins.get_means(),color="xkcd:blue",label='Receive')
-		#lines["rx"] = axes["times"].plot(bandwidth_data["times"]["raw"],bandwidth_data["rx"]["Mbps"],color="xkcd:blue",label='Receive')
 		lines["tx"] = axes["times"].plot(tx_tbins.get_times(),tx_tbins.get_means(),color="xkcd:green",linestyle="--",label='Transmit')
 		fig.subplots_adjust(bottom=0.2)
 		axes["times"].set_xlim(0,24)
@@ -264,27 +283,22 @@ def main():
 		legend_labels = [l.get_label() for l in linesum]
 		axes["times"].legend(linesum,legend_labels,loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
 		chart_filename = "bandwidth_chart.pdf"
-		main_replacement_values["<BANDWIDTH_CHART_NAME>"] = chart_filename
+		report_keyvals.add("bwmonitor/chart_filename", chart_filename)
 		fig.savefig("{}/{}".format(TMP_PATH,chart_filename),format='pdf', bbox_inches='tight')
 
-		with open("{}/bandwidth_report_template.tex".format(REPORT_TEMPLATE_PATH),"r") as f:
-			bandwidth_tex = f.read()
+		##with open("{}/bandwidth_report_template.tex".format(REPORT_TEMPLATE_PATH),"r") as f:
+		##	bandwidth_tex = f.read()
 
-		replacement_values={"<CHART_FILENAME>" : chart_filename, "<BIN_WIDTH>" : str(bin_width)}
+		###with open("{}/bandwidth_report.tex".format(TMP_PATH),"w") as f:
+		##	f.truncate()
+		##	f.write(bandwidth_tex)
+		##	f.close()
 
-		for key in replacement_values:
-			bandwidth_tex = bandwidth_tex.replace(key,replacement_values[key])
-
-		with open("{}/bandwidth_report.tex".format(TMP_PATH),"w") as f:
-			f.truncate()
-			f.write(bandwidth_tex)
-			f.close()
-
-		bandwidth_report_macro= "\\input{{{}}}\n".format("bandwidth_report.tex")
-		main_replacement_values["<BANDWIDTH_REPORT>"] = bandwidth_report_macro
+		##bandwidth_report_macro= "\\input{{{}}}\n".format("bandwidth_report.tex")
+		report_keyvals.add("bwmonitor/readings", "True")
 	else:
-		report_log.info("No bandwidth data available for date {}".format(query_date.strftime("%Y-%m-%d")))
-		main_replacement_values["<BANDWIDTH_REPORT>"] = ""
+		report_keyvals.add("bwmonitor/readings", "False")
+		report_log.info("No bandwidth usage data available for date {}".format(query_date.strftime("%Y-%m-%d")))
 
 	max_dns_failures = 1
 	rows = db.get_dns_data(query_date)
@@ -347,13 +361,13 @@ def main():
 		legend_labels = [l.get_label() for l in linesum]
 		axes["query_times"].legend(linesum,legend_labels,loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
 		chart_filename = "dns_chart.pdf"
-		main_replacement_values["<DNS_CHART_NAME>"] = chart_filename
+		report_keyvals.add("main/dns_chart_name", chart_filename)
 		fig.savefig("{}/{}".format(TMP_PATH,chart_filename),format='pdf', bbox_inches='tight')
 
 	# get list of interfaces that have iperf3 records on the query date
 	rows = db.get_iperf3_interfaces(query_date)
 
-	# generate iperf3 reports for each interface
+	# generate graph image files and keyvalues for each interface
 	iperf3_interfaces=[]
 	for r in rows:
 		iperf3_data={}
@@ -371,7 +385,6 @@ def main():
 		iperf3_data["outages"] = {}
 		iperf3_data["outages"]["times"] = []
 		iperf3_data["averages"] = {}
-		#remote_host = r["remote_host"]
 		iperf3_interfaces.append(iperf3_data["remote_host"])
 		iperf3_rows = db.get_iperf3_interface_data(query_date,iperf3_data["remote_host"])
 		(fig,ax) = plt.subplots()
@@ -446,25 +459,20 @@ def main():
 		fig.savefig("{}/{}".format(TMP_PATH,chart_filename),format='pdf', bbox_inches='tight')
 		plt.cla()
 
-		with open("{}/interface_report_template.tex".format(REPORT_TEMPLATE_PATH),"r") as f:
-			interface_tex = f.read()
+		interface_name = iperf3_data["remote_host"]
 
-		replacement_values={ \
-			"<CHART_FILENAME>" : chart_filename, \
-			"<INTERFACE_NAME>" : iperf3_data["remote_host"], \
-			"<RX_MBPS_AVG>" : str(round(float(iperf3_data["averages"]["rx_Mbps"]),2)), \
-			"<TX_MBPS_AVG>" : str(round(float(iperf3_data["averages"]["tx_Mbps"]),2)), \
-			"<RETRANSMITS_AVG>" : str(int(round(iperf3_data["averages"]["retransmits"],0))), \
-			"<OUTAGE_INTERVALS>" : str(len(iperf3_data["outages"]["times"])), \
-			"<OUTAGE_INFO>" : iperf3_data["outages"]["info"]}
+		detokenized_if_name="\\detokenize{{{}}}".format(interface_name)
+		report_keyvals.add("interfaces/{}/chart_filename".format(detokenized_if_name), chart_filename)
+		report_keyvals.add("interfaces/{}/rx_mbps_avg".format(detokenized_if_name), str(round(float(iperf3_data["averages"]["rx_Mbps"]),2)))
+		report_keyvals.add("interfaces/{}/tx_mbps_avg".format(detokenized_if_name), str(round(float(iperf3_data["averages"]["tx_Mbps"]),2)))
+		report_keyvals.add("interfaces/{}/retransmits_avg".format(detokenized_if_name), str(int(round(iperf3_data["averages"]["retransmits"],0))))
+		report_keyvals.add("interfaces/{}/outage_intervals".format(detokenized_if_name), str(len(iperf3_data["outages"]["times"])))
+		report_keyvals.add("interfaces/{}/outage_info".format(detokenized_if_name), iperf3_data["outages"]["info"])
+		report_keyvals.add("interfaces/{}/report_name".format(detokenized_if_name), "{}_iperf3_report".format(interface_name))
 
-		for key in replacement_values:
-			interface_tex = interface_tex.replace(key,replacement_values[key])
-
-		with open("{}/{}_iperf3_report.tex".format(TMP_PATH,iperf3_data["remote_host"]),"w") as f:
-			f.truncate()
-			f.write(interface_tex)
-			f.close()
+	interface_names = ""
+	interface_names = ",".join(iperf3_interfaces)
+	report_keyvals.add("interfaces/interface_names", interface_names)
 
         st_data_usage = db.get_speedtest_data_usage(datetime.today())
         test_count = st_data_usage[0]["test_count"]
@@ -498,56 +506,55 @@ def main():
 	else:
 		quota_warning = ""
 
-	main_replacement_values["<RX_MBPS_AVG>"] = str(round(speedtest_data["averages"]["rx_Mbps"],2))
-	main_replacement_values["<TX_MBPS_AVG>"] = str(round(speedtest_data["averages"]["tx_Mbps"],2))
-	main_replacement_values["<LATENCY_AVG>"] = str(round(speedtest_data["averages"]["ping"],2))
-	main_replacement_values["<RX_MB>"] = str(round(float(rx_bytes)/float(1e6),2))
-	main_replacement_values["<TX_MB>"] = str(round(float(tx_bytes)/float(1e6),2))
-	main_replacement_values["<RXTX_MB>"] = str(round(float(rxtx_MB),2))
-	main_replacement_values["<RXTX_AVG_MB>"] = str(round(avg_rxtx_MB,2))
-	main_replacement_values["<DATA_USAGE_GB>"] = data_usage_GB_text
-	main_replacement_values["<DATA_USAGE_UNITS>"] = data_usage_GB_units
-	main_replacement_values["<DATA_QUOTA_GB>"] = data_usage_quota_GB_text
-	main_replacement_values["<DATA_QUOTA_UNITS>"] = data_usage_quota_units
-	main_replacement_values["<QUOTA_WARNING>"] = quota_warning
+	report_keyvals.add("main/metrics/rx_mbps_avg", str(round(speedtest_data["averages"]["rx_Mbps"],2)))
+	report_keyvals.add("main/metrics/tx_mbps_avg", str(round(speedtest_data["averages"]["tx_Mbps"],2)))
+	report_keyvals.add("main/metrics/latency_avg", str(round(speedtest_data["averages"]["ping"],2)))
+	report_keyvals.add("main/metrics/rx_mb", str(round(float(rx_bytes)/float(1e6),2)))
+	report_keyvals.add("main/metrics/tx_mb", str(round(float(tx_bytes)/float(1e6),2)))
+	report_keyvals.add("main/metrics/rxtx_mb", str(round(float(rxtx_MB),2)))
+	report_keyvals.add("main/metrics/rxtx_avg_mb", str(round(avg_rxtx_MB,2)))
 
-	interface_reports_macro = ""
-	for i in iperf3_interfaces:
-		if_report_name = "{}_iperf3_report".format(i)
-		interface_reports_macro += "\\input{{{}}}\n".format(if_report_name)
-	main_replacement_values["<INTERFACE_REPORTS>"] = interface_reports_macro
+	report_keyvals.add("data_usage/data_usage_gb", data_usage_GB_text)
+	report_keyvals.add("data_usage/data_usage_units", data_usage_GB_units)
+	report_keyvals.add("data_usage/data_quota_gb", data_usage_quota_GB_text)
+	report_keyvals.add("data_usage/data_quota_units", data_usage_quota_units)
+	report_keyvals.add("data_usage/quota_warning", quota_warning)
 
-	for key in main_replacement_values:
-		report = report.replace(key,main_replacement_values[key])
-
-	report_filename="{}/{}_{}_netperf.tex".format(TMP_PATH,CLIENT_ID,query_date.strftime("%Y%m%d"))
-	output = open(report_filename,"w")
-	output.truncate()
-	output.write(report)
-	output.close()
+	# write report keyvalues to .tex file
+	with open("{}/{}".format(TMP_PATH,"report_keyvalues.tex"),"w") as f:
+		f.truncate()
+		f.write(str(report_keyvals))
+		f.close()
 
 	# compile the report
-	cmd="/usr/bin/rubber --pdf --into {} {}".format(REPORTS_PATH,report_filename)
-	##cmd="/usr/bin/rubber -p --into {} {}".format(REPORTS_PATH,report_filename)
-	ps = Popen(cmd,shell=True,stdout=PIPE,stderr=STDOUT)
-	(cmd_results,return_code) = ps.communicate()
+	report_filename="{}_{}_netperf".format(CLIENT_ID,query_date.strftime("%Y%m%d"))
+	cmd="cd {} && /usr/bin/pdflatex -output-directory={} -jobname={} {}".format(TMP_PATH,REPORTS_PATH,report_filename,"{}/{}".format(TMP_PATH,REPORT_TEMPLATE_FILENAME))
 
-	# clean up additional files generated by the compiler
-	cmd="/usr/bin/rubber --clean --into {} {}".format(REPORTS_PATH,report_filename)
-	ps = Popen(cmd,shell=True,stdout=PIPE,stderr=STDOUT)
-	(cmd_results,return_code) = ps.communicate()
+	# LaTeX packages such as longtable sometimes require more than one compile, so try up to 3 times if needed.
+	compile_attempts = 0
+	compiled = False
+	while (compiled == False and compile_attempts < 3):
+		ps = Popen(cmd,shell=True,stdout=PIPE,stderr=STDOUT)
+		(cmd_results,return_code) = ps.communicate()
+		if not cmd_results.lower().find("rerun"):
+			compiled = True
+		compile_attempts += 1
 
-	# clean up temporary files created during report generation
-	tmp_files=[]
-	for root, dirs, files in os.walk(TMP_PATH):
-	    for f in files:
-	        if f.endswith((".png", ".tex",".aux",".log",".svg",".pdf")):
-	            tmp_files.append(os.path.join(root, f))
-	for f in tmp_files:
-		try:
-			os.remove(f)
-		except:
-			print "Error: unable to delete file {}".format(f)
+	# clean up the temporary files created during report generation
+	# note: I have disabled this for now; the intermediate files and logs are very useful for debugging purposes.
+	#       As the project matures I will enable temporary file clean up. It might be worth adding a command-line
+	#       option to preserve the temporary files.
+	#
+	#tmp_files=[]
+	#for root, dirs, files in os.walk(TMP_PATH):
+	#    for f in files:
+	#        if f.endswith((".png", ".tex",".aux",".log",".svg",".pdf")):
+	#            tmp_files.append(os.path.join(root, f))
+	#for f in tmp_files:
+	#	try:
+	#		os.remove(f)
+	#	except:
+	#		print "Error: unable to delete file {}".format(f)
 
 if __name__ == "__main__":
 	main()
